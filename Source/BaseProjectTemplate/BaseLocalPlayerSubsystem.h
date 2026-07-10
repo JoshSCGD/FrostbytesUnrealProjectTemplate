4186288f3dd6ff813452b114ca954a7ee81b3551
Josh <joshscgd@gmail.com>
2026-07-09 17:53:48 -0700

    updated git ignore and added subsystems

---
+++ Source/BaseProjectTemplate/BaseLocalPlayerSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/LocalPlayerSubsystem.h"
+#include "BaseLocalPlayerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable)
+class BASEPROJECTTEMPLATE_API UBaseLocalPlayerSubsystem : public ULocalPlayerSubsystem
+{
+	GENERATED_BODY()
+	
+};
