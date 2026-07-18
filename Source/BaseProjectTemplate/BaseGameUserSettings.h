0d781dfe3c7584382bfefa7cc3b32b78aea2ad9c
Joshua Shulman-Cass <joshscgd@gmail.com>
2026-07-17 21:34:32 -0700

    added a custom game user settings

---
+++ Source/BaseProjectTemplate/BaseGameUserSettings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "BaseGameUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable)
+class BASEPROJECTTEMPLATE_API UBaseGameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+	
+};
