e3a2d9c55e519a9ad9b4882b5470e0af5e54edd3
Joshua Shulman-Cass <joshscgd@gmail.com>
2026-07-25 18:02:05 -0700

    fixed missing files

---
+++ Source/BaseProjectTemplate/BaseGameInstanceSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "BaseGameInstanceSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable)
+class BASEPROJECTTEMPLATE_API UBaseGameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
