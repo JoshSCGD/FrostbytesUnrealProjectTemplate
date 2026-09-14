5178a4664e3139c713e24f5e88ce5b95634d6064
Joshua Shulman-Cass <joshscgd@gmail.com>
2026-09-14 14:30:25 -0700

    deleted objs

--- Source/BaseProjectTemplate/GameSettingCollection.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "GameSettingCollection.h"
-
-TSubclassOf<UGameSetting> UGameSettingCollection ::GetVisualLookupClass_Implementation() const
-{
-	return UGameSettingCollection::StaticClass();
-}
